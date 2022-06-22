731361a76585266201621015b7abe37c1e4036c0
Danimar Varisa <danimarvarisa@gmail.com>
2022-06-21 21:56:06 -0300

    Add Cliente resources and other adjustments

---
+++ init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE webstore_development;
+CREATE DATABASE webstore_test;
+CREATE DATABASE webstore_production;
--- webstore/config/routes.rb
+++ webstore/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clientes
   resources :produtos
   resources :fornecedores
   get 'welcome/index'
